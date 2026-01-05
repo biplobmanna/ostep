8c2c7f71e86859399cc900ea9c37e4bbd851877a
biplobmanna <biplobmanna@outlook.com>
2026-01-05 10:53:10 +0530

    study-session: 2026-01-05 10:00 AM
    Progress:
    
    - Chapter 1:
    	- Revision
    	- CPU
    	- MEM (70% complete)
    - getting familiar with C code
    - understanding side-effects of C code, and inbuilt functions

--- 01-intro/mem.c
+++ 01-intro/mem.c
@@ -5,19 +5,36 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
+  int *p;                  // init a integer pointer
+  p = malloc(sizeof(int)); // allocate 4b of memory
+  assert(p != NULL);       // assert allocation
+
+  if (argc < 2) {
+    fprintf(stderr, "usage: mem <value>\n"); // fprintf takes a FILE pointer to
+                                             // print to; printing to STDERR
+    fprintf(stdout, "defaulting to: mem 0\n"); // printing to STDOUT
+    *p = 0;
+  } else if (argc == 2) {
+    *p = atoi(argv[1]); // assign value to addr stored in p
+    // if anything other than valid integer is provided, then defaults to 0, or
+    // -1 if alphabet, defaults to 0 if out of bounds int, default to -1 if
+    // float, double, truncates to int
+  } else if (argc >= 3) {
     fprintf(stderr, "usage: mem <value>\n");
-    exit(1);
+    fprintf(stdout, "utilising only the first arg...\n");
+    fprintf(stdout, "running: mem %s\n",
+            argv[1]);   // the argv params are of type char*
+    *p = atoi(argv[1]); // assign value to addr stored in p
   }
-  int *p;
-  p = malloc(sizeof(int));
-  assert(p != NULL);
-  printf("(%d) addr pointed to by p: %p\n", (int)getpid(), p);
-  *p = atoi(argv[1]); // assign value to addr stored in p
+
+  printf("(%d) addr pointed to by p: %p\n", (int)getpid(),
+         p);                                     // print PID and mem LOC
+  printf("(%d) value of p: %d\n", getpid(), *p); // print the value
   while (1) {
-    Spin(1);
-    *p = *p + 1;
-    printf("(%d) value of p: %d\n", getpid(), *p);
+    Spin(1);                                       // Sleep for 1 secs
+    *p = *p + 1;                                   // INC value inside p
+    printf("(%d) value of p: %d\n", getpid(), *p); // print the value
   }
+
   return 0;
 }
--- notes/scratchpad.md
+++ notes/scratchpad.md
@@ -21,3 +21,26 @@ therein to make the best of it.
 
 Working through the homework: copy the respective folder, and
 complete the homework. Make modifications as needed.
+
+---
+
+### C and H Files
+
+The header files must have a corresponding `.c` files if they define any custom
+C-code in it. In this specific case, the C-file must also be part of the
+compilation step.
+
+---
+
+### Testing in C
+
+Since C does not have default testing suite, it will be fun to write up a basic
+test framework for C, to be specifically used here in the ostep project.
+
+#### TODO
+
+- go through some examples of how tests are structured, and/or written
+- create an arch diagram for the test suite
+- write MVP
+- write tests
+- refactor and improve
